0e838727940ccb6e2aece93e44de55f25dd7fddc
Artinger <artinger.tb@gmail.com>
2022-10-03 15:07:28 -0400

    Added a new control scheme in which turning disables other forms of movement while turning, and kept the original control scheme as a backup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverIdea2.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "DriverIdea2")
+public class DriverIdea2 extends LinearOpMode{
+    @Override
+    public void runOpMode() {
+
+        DcMotor frontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            double frontLeftPower;
+            double backLeftPower;
+            double frontRightPower;
+            double backRightPower;
+
+            if (rx > 0) {
+                frontLeftPower = rx;
+                backLeftPower = rx;
+                frontRightPower = -rx;
+                backRightPower = -rx;
+            } else if (rx < 0) {
+                frontLeftPower = rx;
+                backLeftPower = rx;
+                frontRightPower = -rx;
+                backRightPower = -rx;
+            } else {
+                frontLeftPower = (y + x + rx) / denominator;
+                backLeftPower = (y - x + rx) / denominator;
+                frontRightPower = (y - x - rx) / denominator;
+                backRightPower = (y + x - rx) / denominator;
+            }
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+        }
+    }
+}
